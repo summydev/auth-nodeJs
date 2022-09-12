35f76235bf008573936f8b7553f479af9ffaca01
summydev <adegbitesumayah@gmail.com>
2022-09-12 10:54:04 +0100

    changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 app.use(express.static("public"));
@@ -21,10 +21,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ["password"],
+// });
 
 const User = new mongoose.model("user", userSchema);
 app.get("/", function (req, res) {
@@ -35,7 +35,7 @@ app.get("/login", function (req, res) {
 });
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
       console.log(err);
